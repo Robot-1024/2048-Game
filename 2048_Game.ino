13ca3ab58b9e8c3c667fab08dab3bdd25ad7a512
Robot-1024 <128755552+Robot-1024@users.noreply.github.com>
2025-04-23 19:54:09 +0800

    Update 2048_Game.ino

--- 2048_Game.ino
+++ 2048_Game.ino
@@ -1,3 +1,14 @@
+void generateNewNumber() {
+
+	int randNum = random(16); 
+		
+	if (randNum == 0) {
+		  return 4;
+	} else {
+			return 2;
+	}
+}
+
 void setup() {
   // put your setup code here, to run once:
 
